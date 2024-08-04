b161b646a5879702d81ba7f64891a4f4cea0965f
Cristiano Ronaldo <mehdi.nz@hotmail.fr>
2024-08-04 21:49:20 +0200

    okok

--- ja3.go
+++ ja3.go
@@ -52,14 +52,15 @@ func DefaultTlsSpecifications(navigator string) *TlsSpecifications {
 		recordSizeLimit = 0x4001
 	default:
 		signatureAlg = []tls.SignatureScheme{
-			tls.ECDSAWithP256AndSHA256,
+
 			tls.PSSWithSHA256,
 			tls.PKCS1WithSHA256,
-
+			tls.ECDSAWithP384AndSHA384,
 			tls.PSSWithSHA384,
 			tls.PKCS1WithSHA384,
 			tls.PSSWithSHA512,
 			tls.PKCS1WithSHA512,
+			tls.ECDSAWithP256AndSHA256,
 		}
 
 		supportedVersions = []uint16{
