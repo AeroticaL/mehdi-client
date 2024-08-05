242ee30efac38abfdd5232f14f5771e99c5cf936
Cristiano Ronaldo <mehdi.nz@hotmail.fr>
2024-08-05 20:23:52 +0200

    ok

--- ja3.go
+++ ja3.go
@@ -507,11 +507,7 @@ func getSupportedAlgorithms(navigator string) []tls.SignatureScheme {
 			tls.ECDSAWithP256AndSHA256,
 			tls.ECDSAWithP384AndSHA384,
 			tls.ECDSAWithP521AndSHA512,
-			tls.PSSWithSHA256,
-			tls.PSSWithSHA384,
-			tls.PSSWithSHA512,
-			tls.PKCS1WithSHA256,
-			tls.PKCS1WithSHA384,
+
 			tls.PKCS1WithSHA512,
 		}
 	default: //chrome
