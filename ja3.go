99954d5ede92409d5956ba7edaaa72fb05ccbf79
Cristiano Ronaldo <mehdi.nz@hotmail.fr>
2024-08-05 15:49:37 +0200

    s

--- ja3.go
+++ ja3.go
@@ -3,10 +3,9 @@ package azuretls
 import (
 	"errors"
 	"fmt"
+	tls "github.com/Noooste/utls"
 	"strconv"
 	"strings"
-
-	tls "github.com/Noooste/utls"
 )
 
 // TlsSpecifications struct contains various fields representing TLS handshake settings.
@@ -36,6 +35,13 @@ func DefaultTlsSpecifications(navigator string) *TlsSpecifications {
 			tls.ECDSAWithP384AndSHA384,
 			tls.ECDSAWithP521AndSHA512,
 			tls.PSSWithSHA256,
+			tls.PSSWithSHA384,
+			tls.PSSWithSHA512,
+			tls.PKCS1WithSHA256,
+			tls.PKCS1WithSHA384,
+			tls.PKCS1WithSHA512,
+			tls.ECDSAWithSHA1,
+			tls.PKCS1WithSHA1,
 		}
 		supportedVersions = []uint16{
 			tls.VersionTLS13,
