0abf52228838277dd16892dbf42687a408e16f1e
Cristiano Ronaldo <mehdi.nz@hotmail.fr>
2024-08-06 13:54:51 +0200

    dem

--- presets.go
+++ presets.go
@@ -1,8 +1,9 @@
 package azuretls
 
 import (
-	"github.com/Noooste/fhttp/http2"
 	"math"
+
+	"github.com/Noooste/fhttp/http2"
 )
 
 const (
@@ -19,7 +20,7 @@ func defaultHeaderSettings(navigator string) (map[http2.SettingID]uint32, []http
 	switch navigator {
 	case Firefox:
 		return map[http2.SettingID]uint32{
-				http2.SettingMaxFrameSize:      16384,
+				http2.SettingMaxFrameSize:      1638,
 				http2.SettingInitialWindowSize: 131072,
 				http2.SettingHeaderTableSize:   65536,
 			}, []http2.SettingID{
